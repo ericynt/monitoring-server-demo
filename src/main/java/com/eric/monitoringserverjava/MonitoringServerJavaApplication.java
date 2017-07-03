1a4f095cbe425a76d774d64f9a82faac2b5f3d59
Eric Yntema <e.yntema@gmail.com>
2017-07-03 14:21:47 +0200

    Initial commit.

---
+++ src/main/java/com/eric/monitoringserverjava/MonitoringServerJavaApplication.java
@@ -0,0 +1,12 @@
+package com.eric.monitoringserverjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringServerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringServerJavaApplication.class, args);
+	}
+}
